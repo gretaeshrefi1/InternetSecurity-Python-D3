ae587c54965919ce89882eb89f3593bfdc2d79d4
Greta <gretaeshrefi@gmail.com>
2021-02-13 20:56:15 +0100

    testingg

--- detyra3.py
+++ detyra3.py
@@ -1,25 +1,31 @@
 from tkinter import *
+# from Pillow import ImageTk,Image
+import tkinter
 
 root = Tk()
 root.title("Aplikacioni per detyren e trete")
-root.geometry("400x400")
+root.iconbitmap('up.png')
+root.geometry("500x400")
 
- 
-
-
+# logo = ImageTk.PhotoImage(Image.open("up.png"))
+# labela = Label(image=logo)
+# labela.pack()
 
 my_menu = Menu(root)
 root.config(menu=my_menu)
 
-#click command
 def our_command():
 	hide_frames()
-	my_label = Label(root, text = "You clicked a dropdown menu..").pack()
+	teksti = Label(root, text = "You clicked a dropdown menu..").pack()
 
 def fk():
 	hide_frames()
 	file_new_frame.pack(fill="both", expand=1)
-	my_label = Label(file_new_frame, text = "University of Prishtina 'Hasan Prishtina'").pack()
+	teksti = Label(file_new_frame, text = "Universiteti i Prishtinës 'Hasan Prishtina'").pack()
+	teksti = Label(file_new_frame, text = "Viti 3 ").pack()
+	teksti = Label(file_new_frame, text = "Lënda: 'Siguria e Internetit'").pack()
+	teksti = Label(file_new_frame, text = "Profesori i lëndës: Blerim Rexha").pack()
+	teksti = Label(file_new_frame, text = "Asistenti i lëndës: Arbnor Halili").pack()
 
 def students():
 	hide_frames()
@@ -27,18 +33,18 @@ def students():
 	teksti = Label(file_new_frame, text="Studentet: ").pack()
 	teksti = Label(file_new_frame, text="Arianita Kabashi").pack()
 	teksti = Label(file_new_frame, text="Etnika Halili").pack()
-	teksti = Label(file_new_frame, text="Greta Eshrefi").pack()
+	teksti = Label(file_new_frame, text="Gretë Eshrefi").pack()
 
 def projekti():
 	hide_frames()
 	file_new_frame.pack(fill="both", expand=1)
-	my_label = Label(file_new_frame, text = "Projekti i trete ne lenden siguria e Internetit ka te beje me....").pack()
+	teksti = Label(file_new_frame, text = "Projekti i trete ne lenden siguria e Internetit ka te beje me krijimin e nje Aplikacioni.").pack()
+	teksti = Label(file_new_frame, text = "Ky aplikacion duhet te permbaje nje meny per perzgjedhjen e opcioneve te ndryshme.").pack()
+	teksti = Label(file_new_frame, text = "Menyja duhet te jete e punuar si GUI duke perdorur vetem modulin tkinter te Python.").pack()
 
-#hide all frames function
 def hide_frames():
 	file_new_frame.pack_forget()
 	edit_cut_frame.pack_forget()
-
 
 
 #create a menu item
@@ -54,14 +60,18 @@ file_menu.add_command(label="Project", command=projekti)
 #create another menu
 edit_menu = Menu (my_menu)
 my_menu.add_cascade(label="Show", menu=edit_menu)
-edit_menu.add_command(label="Show passwords", command=our_command)
-# file_menu.add_separator()
-# edit_menu.add_command(label="Copy", command=our_command)
+edit_menu.add_command(label="Show passwords from Google Chrome", command=our_command)
+file_menu.add_separator()
+edit_menu.add_command(label="Show passwords from Mozilla Firefox", command=our_command)
 
 
 #create some frames
 file_new_frame = Frame(root, width=400, height=400, bg="grey")
 edit_cut_frame = Frame(root, width=400, height=400, bg="pink")
+
+
+butoni = Button(root, text="Dil nga programi", command=root.quit)
+butoni.pack(side=tkinter.BOTTOM)
 
 root.mainloop()
 
@@ -69,32 +79,3 @@ root.mainloop()
 
 
 
-
-
-# import os
-# import sqlite3
-# # import win32crypt
-
-
-# def get_chrome():
-#     data_path = os.path.expanduser('~') + r'\AppData\Local\Google\Chrome\User Data\Default\Login Data'
-#     c = sqlite3.connect(data_path)
-#     cursor = c.cursor()
-#     select_statement = 'SELECT origin_url, username_value, password_value FROM logins'
-#     cursor.execute(select_statement)
-
-#     login_data = cursor.fetchall()
-
-#     cred = {}
-
-#     string = ''
-
-#     for url, user_name, pwd in login_data:
-#         pwd = win32crypt.CryptUnprotectData(pwd)
-#         cred[url] = (user_name, pwd[1].decode('utf8'))
-#         string += '\n[+] URL:%s USERNAME:%s PASSWORD:%s\n' % (url,user_name,pwd[1].decode('utf8'))
-#         print(string)
-
-
-# if _name__=='__main_':
-#     get_chrome()
