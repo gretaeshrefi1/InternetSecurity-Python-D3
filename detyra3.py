be51548c900e7ae054bf1c8f2c19833966e4f768
Greta <gretaeshrefi@gmail.com>
2021-02-13 21:49:21 +0100

    nd finale

--- detyra3.py
+++ detyra3.py
@@ -1,18 +1,17 @@
-from tkinter import *
-# from Pillow import ImageTk,Image
 import tkinter
+from tkinter import *
+from tkinter import ttk
+
 
 root = Tk()
 root.title("Aplikacioni per detyren e trete")
 root.iconbitmap('up.png')
-root.geometry("500x400")
+root.geometry("470x400")
 
-# logo = ImageTk.PhotoImage(Image.open("up.png"))
-# labela = Label(image=logo)
-# labela.pack()
 
 my_menu = Menu(root)
 root.config(menu=my_menu)
+
 
 def our_command():
 	hide_frames()
@@ -20,54 +19,57 @@ def our_command():
 
 def fk():
 	hide_frames()
-	file_new_frame.pack(fill="both", expand=1)
-	teksti = Label(file_new_frame, text = "Universiteti i Prishtinës 'Hasan Prishtina'").pack()
-	teksti = Label(file_new_frame, text = "Viti 3 ").pack()
-	teksti = Label(file_new_frame, text = "Lënda: 'Siguria e Internetit'").pack()
-	teksti = Label(file_new_frame, text = "Profesori i lëndës: Blerim Rexha").pack()
-	teksti = Label(file_new_frame, text = "Asistenti i lëndës: Arbnor Halili").pack()
+	korniza.pack(fill="both", expand=1)
+	teksti = Label(korniza, text = "Universiteti i Prishtinës 'Hasan Prishtina'").pack()
+	teksti = Label(korniza, text = "Departamenti: Inxhinieri Kompjuterike").pack()
+	teksti = Label(korniza, text = "Viti 3").pack()
+	teksti = Label(korniza, text = "Lënda: 'Siguria e Internetit'").pack()
+	teksti = Label(korniza, text = "Profesori i lëndës: Blerim Rexha").pack()
+	teksti = Label(korniza, text = "Asistenti i lëndës: Arbnor Halili").pack()
+	teksti = Label(korniza, text="").pack()
 
 def students():
 	hide_frames()
-	file_new_frame.pack(fill="both", expand=1)
-	teksti = Label(file_new_frame, text="Studentet: ").pack()
-	teksti = Label(file_new_frame, text="Arianita Kabashi").pack()
-	teksti = Label(file_new_frame, text="Etnika Halili").pack()
-	teksti = Label(file_new_frame, text="Gretë Eshrefi").pack()
+	korniza.pack(fill="both", expand=1)
+	teksti = Label(korniza, text="Studentet: ").pack()
+	teksti = Label(korniza, text="Arianita Kabashi").pack()
+	teksti = Label(korniza, text="Etnika Halili").pack()
+	teksti = Label(korniza, text="Gretë Eshrefi").pack()
+	teksti = Label(korniza, text="").pack()
 
 def projekti():
 	hide_frames()
-	file_new_frame.pack(fill="both", expand=1)
-	teksti = Label(file_new_frame, text = "Projekti i trete ne lenden siguria e Internetit ka te beje me krijimin e nje Aplikacioni.").pack()
-	teksti = Label(file_new_frame, text = "Ky aplikacion duhet te permbaje nje meny per perzgjedhjen e opcioneve te ndryshme.").pack()
-	teksti = Label(file_new_frame, text = "Menyja duhet te jete e punuar si GUI duke perdorur vetem modulin tkinter te Python.").pack()
+	korniza.pack(fill="both", expand=1)
+	teksti = Label(korniza, text="").pack()
+	teksti = Label(korniza, text = "Projekti i trete ne lenden siguria e Internetit ka te beje me krijimin e nje Aplikacioni.").pack()
+	teksti = Label(korniza, text = "Ky aplikacion duhet te permbaje nje meny per perzgjedhjen e opcioneve te ndryshme.").pack()
+	teksti = Label(korniza, text = "Menyja duhet te jete e punuar si GUI duke perdorur vetem modulin tkinter te Python.").pack()
+	teksti = Label(korniza, text="").pack()
 
 def hide_frames():
-	file_new_frame.pack_forget()
-	edit_cut_frame.pack_forget()
+	korniza.pack_forget()
+	teksti.pack_forget()
 
 
-#create a menu item
-file_menu = Menu(my_menu)
-my_menu.add_cascade(label="About", menu=file_menu)
-file_menu.add_command(label="University", command=fk)
-file_menu.add_separator()
-file_menu.add_command(label="Students", command=students)
-file_menu.add_separator()
-file_menu.add_command(label="Project", command=projekti)
+#krijimi i menyse se pare "About"
+about = Menu(my_menu)
+my_menu.add_cascade(label="About", menu=about)
+about.add_command(label="University", command=fk)
+about.add_separator()
+about.add_command(label="Students", command=students)
+about.add_separator()
+about.add_command(label="Project", command=projekti)
+
+#krijimi i menyse se dyte "Show"
+show_menu = Menu (my_menu)
+my_menu.add_cascade(label="Show", menu=show_menu)
+show_menu.add_command(label="Show passwords from Google Chrome", command=our_command)
+about.add_separator()
+show_menu.add_command(label="Show passwords from Mozilla Firefox", command=our_command)
 
 
-#create another menu
-edit_menu = Menu (my_menu)
-my_menu.add_cascade(label="Show", menu=edit_menu)
-edit_menu.add_command(label="Show passwords from Google Chrome", command=our_command)
-file_menu.add_separator()
-edit_menu.add_command(label="Show passwords from Mozilla Firefox", command=our_command)
-
-
-#create some frames
-file_new_frame = Frame(root, width=400, height=400, bg="grey")
-edit_cut_frame = Frame(root, width=400, height=400, bg="pink")
+korniza = Frame(root, width=400, height=400, bg="light blue")
+teksti = Label(root)
 
 
 butoni = Button(root, text="Dil nga programi", command=root.quit)
